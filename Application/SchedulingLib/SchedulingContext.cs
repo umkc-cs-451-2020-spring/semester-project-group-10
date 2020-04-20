1c1c7397c1c1768610928c2d69775a3961d74906
BigWendl <37221842+BigWendl@users.noreply.github.com>
2020-04-20 09:57:17 -0500

    Created Database Structure in C# code (#23) (#25)

--- Application/SchedulingLib/SchedulingContext.cs
+++ Application/SchedulingLib/SchedulingContext.cs
@@ -8,7 +8,12 @@ namespace SchedulingLib
 {
     class SchedulingContext : DbContext
     {
+        public DbSet<Building> Buildings { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<Section> Sections { get; set; }
+        public DbSet<Course> Courses { get; set; }
         public DbSet<Instructor> Instructors { get; set; }
+
         protected override void OnConfiguring(DbContextOptionsBuilder options)
             => options.UseSqlite("Data Source=courseSchedule.db");
     }
---
+++ Application/SchedulingLib/models/DatabaseModels.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchedulingLib.models
+{
+    public class Building
+    {
+        public int BuildingId { get; set; }
+        public string BuildingName { get; set; }
+
+        public List<Room> Rooms { get; set; }
+    }
+
+    public class Room
+    {
+        public int RoomId { get; set; }
+        public int RoomSize { get; set; }
+        public string RoomNumber { get; set; }
+
+        public int BuildingId { get; set; }
+        public Building Building { get; set;}
+
+        public List<Section> Sections { get; set; }
+    }
+
+    public class Course
+    {
+        public int CourseId { get; set; }
+        public string CourseName { get; set; }
+        public int CourseSize { get; set; }
+
+        public List<Section> Sections { get; set; }
+    }
+
+    public class Instructor
+    {
+        public int InstructorId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public List<Section> Sections { get; set; }
+    }
+
+    public class Section
+    {
+        public int SectionId { get; set; }
+        public string DayOfWeek { get; set; }
+        public int StartTime { get; set; }
+        public int EndTime { get; set; }
+
+        public int RoomId { get; set; }
+        public Room Room { get; set; }
+
+        public int CourseId { get; set; }
+        public Course Course { get; set; }
+
+        public int InstructorId { get; set; }
+        public Instructor Instructor { get; set; }
+
+    }
+}

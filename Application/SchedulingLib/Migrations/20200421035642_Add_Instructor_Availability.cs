52fe094363a745eaece138ed1e7bc00e6a5967a3
Alec Sears <alecxvs@users.noreply.github.com>
2020-04-20 23:00:06 -0500

    Add Migration "Add_Instructor_Availability"

---
+++ Application/SchedulingLib/Migrations/20200421035642_Add_Instructor_Availability.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Models.Migrations
+{
+    public partial class Add_Instructor_Availability : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Availability",
+                table: "Instructors",
+                maxLength: 1,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Availability",
+                table: "Instructors");
+        }
+    }
+}

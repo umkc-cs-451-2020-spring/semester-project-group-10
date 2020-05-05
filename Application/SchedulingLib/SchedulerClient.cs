dcbc0e5ccbd1d8742285d11230b0e8fa45863bad
Josh Wendl <winnetonka68@gmail.com>
2020-05-04 22:02:57 -0500

    Added Add/Edit/Remove of Database Objects

--- Application/SchedulingLib/SchedulerClient.cs
+++ Application/SchedulingLib/SchedulerClient.cs
@@ -15,11 +15,99 @@ namespace SchedulingLib
             context = new SchedulingContext(connectionUrl);
         }
 
+
         public void NewInstructor(Instructor instructor)
         {
             context.Instructors.Add(instructor);
             context.SaveChanges();
         }
+
+        public void NewBuilding(Building building)
+        {
+            context.Buildings.Add(building);
+            context.SaveChanges();
+        }
+
+        public void NewRoom(Room room)
+        {
+            context.Rooms.Add(room);
+            context.SaveChanges();
+        }
+
+        public void NewCourse(Course course)
+        {
+            context.Courses.Add(course);
+            context.SaveChanges();
+        }
+
+        public void NewSection(Section section)
+        {
+            context.Sections.Add(section);
+            context.SaveChanges();
+        }
+
+
+        public void EditInstructor(Instructor instructor)
+        {
+            context.Instructors.Update(instructor);
+            context.SaveChanges();
+        }
+
+        public void EditBuilding(Building building)
+        {
+            context.Buildings.Update(building);
+            context.SaveChanges();
+        }
+
+        public void EditRoom(Room room)
+        {
+            context.Rooms.Update(room);
+            context.SaveChanges();
+        }
+
+        public void EditCourse(Course course)
+        {
+            context.Courses.Update(course);
+            context.SaveChanges();
+        }
+
+        public void EditSection(Section section)
+        {
+            context.Sections.Update(section);
+            context.SaveChanges();
+        }
+
+
+        public void RemoveInstructor(Instructor instructor)
+        {
+            context.Instructors.Remove(instructor);
+            context.SaveChanges();
+        }
+
+        public void RemoveBuilding(Building building)
+        {
+            context.Buildings.Remove(building);
+            context.SaveChanges();
+        }
+
+        public void RemoveRoom(Room room)
+        {
+            context.Rooms.Remove(room);
+            context.SaveChanges();
+        }
+
+        public void RemoveCourse(Course course)
+        {
+            context.Courses.Remove(course);
+            context.SaveChanges();
+        }
+
+        public void RemoveSection(Section section)
+        {
+            context.Sections.Remove(section);
+            context.SaveChanges();
+        }
+
 
         public async Task<IEnumerable<Instructor>> ListInstructors()
         {
